cdd2119ae0cc77245473f3fe195fa9c096f18471
Vera <moirabestwaifu@gmail.com>
2022-08-06 20:47:13 +0200

    fixed bugs, don't think too much of it

--- index.js
+++ index.js
@@ -2,57 +2,58 @@ let nRounds = 0;
 let playerPoints = 0;
 let robotPoints = 0;
 
-function getComputerChoice(){
+function getComputerChoice() {
     let items = ["rock", "paper", "scissors"]
-    return items[Math.floor((Math.random()*3))]
+    return items[Math.floor((Math.random() * 3))]
 }
 
 function playRound(playerSelection, computerSelection) {
     //playerSelection = playerSelection.toLowerCase()
-    
-    if ((playerSelection == "rock" && computerSelection=="scissors")|| (playerSelection == "paper" && computerSelection=="rock")|| (playerSelection == "scissors" && computerSelection=="paper")){
+
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
         return "player"
     } else if (playerSelection == computerSelection) {
         return "draw"
     } else {
         return "robot"
     }
-  }
+}
 
-function game(playerChoice){
+function game(playerChoice) {
     nRounds++;
 
     let winner = playRound(playerChoice, getComputerChoice());
-        if (winner == "player") {
-            playerPoints ++;
-        } else if (winner == "robot") {
-            robotPoints++;
-        } else {nRounds--} 
+
+    if (winner == "player") {
+        playerPoints++;
+    } else if (winner == "robot") {
+        robotPoints++;
+    } else { nRounds-- }
+
+    console.log(playerPoints + " these are the player points for now")
+    console.log(robotPoints + " these are the robot points for now")
+    console.log(nRounds + " this is the round number...")
+
+    if (nRounds == 5) {
+        playerPoints = 0;
+        robotPoints = 0;
+        nRounds = 0;
+        if (playerPoints > robotPoints) {
+            //return "PLAYER WINS"
+            console.log("PLAYER WINS")
+        } else {
+            //return "ROBOT WINS"
+            console.log("ROBOT WINS")
+        }
+    }
 
 
-        console.log(playerPoints+" these are the player points for now")
-        console.log(robotPoints+" these are the robot points for now")
-        console.log(nRounds+" this is the round number...")
-
-        if (nRounds >= 5){
-            playerPoints = 0;
-            robotPoints = 0;
-            nRounds = 0;
-            if (playerPoints > robotPoints){
-                //return "PLAYER WINS"
-                console.log("PLAYER WINS")
-            } else {
-                //return "ROBOT WINS"
-                console.log("ROBOT WINS")
-            }
-        console.log("Number of rounds:")
-        console.log(nRounds)
-}}
+}
 
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 
-rockButton.addEventListener("click", function(){game("rock");});
-paperButton.addEventListener("click", function(){game("paper");});
-scissorsButton.addEventListener("click", function(){game("scissors");});
+rockButton.addEventListener("click", function () { game("rock"); });
+paperButton.addEventListener("click", function () { game("paper"); });
+scissorsButton.addEventListener("click", function () { game("scissors"); });
